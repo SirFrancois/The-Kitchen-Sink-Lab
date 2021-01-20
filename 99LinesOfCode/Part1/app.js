a20d32f58a1cbfa4d4f3453f120b42c56a6d8ccb
sirfrancois <sirfrancois4@gmail.com>
2021-01-20 13:46:47 -0600

    99 lines of codde

---
+++ 99LinesOfCode/Part1/app.js
@@ -0,0 +1,16 @@
+const friends = ['maliek', 'Chris', 'Eric', 'Dj', 'Ant' ];
+//josh helped with this loop
+
+for (let j = 0; i < friends.length; j++){
+    console.log(friendArray[j] + ":")
+
+//The stuff i wrote before josh helped
+    for (let i = 99; i > 0; i--);
+//the lines above was " i > friends.length" before changes//
+     if(i > 1){
+    console.log(i + "lines of code on the wall" + i + "Lines of code" + friends[i] +  "strikes one down, clears it all out" + (i-1) + "lines of code on the wall")
+    }else{ 
+    console.log(i + "lines of code on the wall" + i + "lines of code" + friends[i] + "strikes one down, clears it all out, no more lines of code in the file")
+
+}
+}
